cc6734b589691d9edc61df245bb7ab8800d85bbc
Marcio Ladislau <marcio.ladislau.am@gmail.com>
2021-02-04 12:42:53 -0400

    Criando operação de instanciação

--- src/main/java/com/marcioladislau/cursomc/CursomcApplication.java
+++ src/main/java/com/marcioladislau/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.marcioladislau.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.marcioladislau.cursomc.categories.CategoriaRepository;
+import com.marcioladislau.cursomc.domain.Categoria;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null,"Industrial");
+		Categoria cat2 = new Categoria(null,"Químico");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
 	}
 
 }
--- src/main/java/com/marcioladislau/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcioladislau/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.marcioladislau.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
